6d89341b1f303ce45d3f207cb40079f4b729aef8
Niyus01 <suyiamigo88@gmail.com>
2020-03-04 19:49:48 -0500

    Conecting the App with PayPal

--- app/src/main/java/com/suyin/books_store/ListActivity.java
+++ app/src/main/java/com/suyin/books_store/ListActivity.java
@@ -15,6 +15,16 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+import com.paypal.android.sdk.payments.PayPalConfiguration;
+import com.paypal.android.sdk.payments.PayPalPayment;
+import com.paypal.android.sdk.payments.PayPalService;
+import com.paypal.android.sdk.payments.PaymentActivity;
+import com.paypal.android.sdk.payments.PaymentConfirmation;
+
+
+import org.json.JSONException;
+
+import java.math.BigDecimal;
 
 import java.util.ArrayList;
 
@@ -25,9 +35,24 @@ public class ListActivity extends AppCompatActivity {
     private Button btnCancel, btnRemove, btnBuy, btnStore;
     private ListView mListView;
     private int selectID;
-    private String selectName;
+    private String selectName, amount;
     private TextView totalPayment;
     private EditText bookSelect;
+
+    public static final String PAYPAL_CLIENT_ID = "AYS1Ahsj-cjU2JKTqfLHFCQ-OyUkf7aBge7La3gkcRK_qCZt_M_8tbb41yb7IQg0QGwNQaEUfKDAkhUe";
+    private static final int PAYPAL_REQUEST_CODE = 7171;
+
+    private static PayPalConfiguration config = new PayPalConfiguration()
+            .environment(PayPalConfiguration.ENVIRONMENT_SANDBOX)
+            .clientId(PAYPAL_CLIENT_ID);
+
+    @Override
+    protected  void onDestroy()
+    {     stopService(new Intent(this, PayPalService.class));
+        super.onDestroy();
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +66,11 @@ public class ListActivity extends AppCompatActivity {
         btnStore = findViewById(R.id.btnStore);
         mListView = findViewById(R.id.listDemo);
         totalPayment =  findViewById(R.id.textPrice);
+
+
+        Intent intent = new Intent (this, PayPalService.class);
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
+        startService(intent);
 
         db = new DataBaseHelperBooks(this);
         enabledFalse();
@@ -61,10 +91,13 @@ public class ListActivity extends AppCompatActivity {
                 double total_payment=0;
                 Toast.makeText(ListActivity.this,"Thanks you enjoy!!",Toast.LENGTH_SHORT).show();
 
-                db.deleteTable();
-                populateListView();
-                btnBuy.setEnabled(false);
-                enabledFalse();
+               // db.deleteTable();
+
+               // btnBuy.setEnabled(false);
+               // enabledFalse();
+                processPayment();
+               // populateListView();
+
 
             }
         });
@@ -125,11 +158,12 @@ public class ListActivity extends AppCompatActivity {
 
         Cursor data = db.getData();
         Cursor dataTotal = db.getPrice();
-        String payment="";
-        double a=0.0;
+
+        double payment=0.0;
         if(dataTotal.getCount()>0) {
             dataTotal.moveToFirst();
-           a = dataTotal.getDouble(dataTotal.getColumnIndex("total"));
+           payment = dataTotal.getDouble(dataTotal.getColumnIndex("total"));
+           amount ="1";// String.valueOf(payment);
         }
         ArrayList<String> listData = new ArrayList<>();
 
@@ -144,8 +178,54 @@ public class ListActivity extends AppCompatActivity {
         //create the list adapter and set the adapter
         ListAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listData);
         mListView.setAdapter(adapter);
-        totalPayment.setText("Current amount: $"+ String.format("%.2f",a));
+        totalPayment.setText("Current amount: $"+ String.format("%.2f",payment));
 
+    }
+
+    private void processPayment(){
+        //amount = totalPayment.getText().toString();
+        PayPalPayment payPalPayment = new PayPalPayment (new BigDecimal(String.valueOf(amount)), "USD",
+                "Pay your balance", PayPalPayment.PAYMENT_INTENT_SALE);
+
+        Intent intent = new Intent (this, PaymentActivity.class);
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
+        intent.putExtra(PaymentActivity.EXTRA_PAYMENT, payPalPayment);
+        startActivityForResult(intent, PAYPAL_REQUEST_CODE);
+
+
+
+    }
+
+    @Override
+    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+
+       // super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PAYPAL_REQUEST_CODE) {
+
+            if (requestCode == RESULT_OK) {
+                PaymentConfirmation confirmation = data.getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
+                if (confirmation != null) {
+                    try {
+                        db.deleteTable();
+                        populateListView();
+                        Intent intent = new Intent(this, PaymentDetailsActivity.class);
+                        String paymentDetails = confirmation.toJSONObject().toString(4);
+                       // db.deleteTable();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        }
+
+        else if(requestCode==RESULT_CANCELED){
+            Toast.makeText(this, "Canceled", Toast.LENGTH_SHORT).show();}
+
+        else if(resultCode== PaymentActivity.RESULT_EXTRAS_INVALID){
+            Toast.makeText(this, "Invalid", Toast.LENGTH_SHORT).show();
+
+        }
     }
 
     public void enabledFalse(){
